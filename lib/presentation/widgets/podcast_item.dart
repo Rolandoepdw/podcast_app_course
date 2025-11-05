b43337e6bfb624ebd95aa5cbbc3ebf26f2d29de4
Rolandoepdw <103588025+Rolandoepdw@users.noreply.github.com>
2025-11-05 16:06:49 -0500

    feat: Show download progress and cache podcast images
    - Add a circular progress indicator to the download button to show the download progress.
    - Change the download button icon to a play icon after the download is complete.
    - Integrate the `cached_network_image` package to cache podcast cover images.
    - Add `cached_network_image` as a new dependency.

--- lib/presentation/widgets/podcast_item.dart
+++ lib/presentation/widgets/podcast_item.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:podcast_app_course/domain/entities/podcast.dart';
 import 'package:podcast_app_course/services/api/podcast_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PodcastItem extends StatefulWidget {
   final Podcast podcast;
@@ -13,30 +15,53 @@ class PodcastItem extends StatefulWidget {
 
 class _PodcastItemState extends State<PodcastItem> {
   final podcastApi = PodcastApi();
+  double progress = 0.0;
+  String? savedPath;
+
+  Future<String?> getSavedPath() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {});
+    return savedPath = sharedPreferences.getString('path');
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(widget.podcast.title),
-      trailing: IconButton(
-        icon: const Icon(Icons.download),
-        onPressed: () async {
-          print(widget.podcast.audio);
-          final path = '${widget.podcast.title}/${widget.podcast.id}.mp3';
-          await podcastApi.downloadPodcast(
-            widget.podcast.audio,
-            path,
-            onReceiveProgress: (count, total) {
-              debugPrint('count: $count, total: $total');
+      trailing: Stack(
+        children: [
+          Positioned.fill(
+            child: CircularProgressIndicator.adaptive(value: progress),
+          ),
+          IconButton(
+            icon: savedPath == null
+                ? const Icon(Icons.download)
+                : const Icon(Icons.play_arrow),
+            onPressed: () async {
+              final path = '${widget.podcast.title}/${widget.podcast.id}.mp3';
+              await podcastApi.downloadPodcast(
+                widget.podcast.audio,
+                path,
+                onReceiveProgress: (count, total) {
+                  debugPrint('count: $count, total: $total');
+                  setState(() => progress = count / total);
+                },
+              );
+              await getSavedPath();
+              setState(() => progress = 0.0);
             },
-          );
-        },
+          ),
+        ],
       ),
-      leading: Image.network(
-        widget.podcast.image,
+      leading: CachedNetworkImage(
+        imageUrl: widget.podcast.image,
         width: 100,
         height: 100,
         fit: BoxFit.cover,
+        placeholder: (context, url) =>
+            ColoredBox(color: Theme.of(context).primaryColor),
+        errorWidget: (context, url, error) =>
+            const Icon(Icons.error, color: Colors.red),
       ),
     );
   }
